625efaf23752db60bb3a7c51f9f1d811da176f2b
villijoe <joe_joe_barabanshi@mail.ru>
2016-04-01 17:26:16 +0300

    feature a dot weight field

--- form_template.php
+++ form_template.php
@@ -1,7 +1,7 @@
 <form style="position: absolute; top: 0; right: 0;" action="add_weight.php" method="post">
     <p>Date</p>
-    <input type="date" required name="date" value="<?php echo date("Y-m-d"); ?>" />
+    <input type="date" required name="date" value="<?php echo date("Y-m-d"); ?>" disabled />
     <p>Weight</p>
-    <input type="text" required id="weight" name="weight" autofocus /><br /><br />
+    <input type="text" maxlength="5" required id="weight" name="weight" autofocus /><br /><br />
     <input type="submit" />
 </form>
